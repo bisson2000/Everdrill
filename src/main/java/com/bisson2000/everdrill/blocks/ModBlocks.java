2eb59bec64234bb6f26f25e549e9d5eea3927b96
Paul-Andre Bisson <bissonpaulandre@gmail.com>
2025-03-26 06:53:41 -0400

    Added recipe

--- src/main/java/com/bisson2000/everdrill/blocks/ModBlocks.java
+++ src/main/java/com/bisson2000/everdrill/blocks/ModBlocks.java
@@ -1,15 +1,23 @@
 package com.bisson2000.everdrill.blocks;
 
 import com.bisson2000.everdrill.Everdrill;
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllMovementBehaviours;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.content.kinetics.BlockStressDefaults;
 import com.simibubi.create.content.kinetics.drill.DrillMovementBehaviour;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.simibubi.create.foundation.data.TagGen;
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBlocks {
 
@@ -25,10 +33,21 @@ public class ModBlocks {
             })
             .transform(BlockStressDefaults.setImpact(4.0))
             .onRegister(AllMovementBehaviours.movementBehaviour(new EverdrillMovementBehavior()))
+            .recipe((c, p) -> {
+                ShapedRecipeBuilder.shaped(RecipeCategory.MISC, c.get())
+                        .define('L', Items.LAPIS_LAZULI)
+                        .define('D', Items.DIAMOND)
+                        .define('M', AllBlocks.MECHANICAL_DRILL.asItem())
+                        .pattern("LDL")
+                        .pattern("DMD")
+                        .pattern("LDL")
+                        .unlockedBy("has_" + c.getName(), RegistrateRecipeProvider.has(c.get()))
+                        .save(p, new ResourceLocation(Everdrill.MOD_ID, c.getName()));
+            })
             .item()
             .tag(AllTags.AllItemTags.CONTRAPTION_CONTROLLED.tag)
             .model((c, p) -> {
-                ResourceLocation itemLocation = new ResourceLocation(Everdrill.MOD_ID, "block/everdrill/source_item");
+                ResourceLocation itemLocation = new ResourceLocation(Everdrill.MOD_ID, "item/everdrill/source_item");
                 ResourceLocation headLocation = new ResourceLocation(Everdrill.MOD_ID, "block/everdrill/source_head");
                 p.withExistingParent(c.getName(), itemLocation);
                 p.withExistingParent("block/" + c.getName() + "/head", headLocation);
