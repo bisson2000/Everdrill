43d1ac33ab0f86e320afe5b5b7dd1d356bf51ab7
Paul-Andre Bisson <bissonpaulandre@gmail.com>
2025-04-21 08:36:27 -0400

    Added kinetic stress impact tooltip

--- src/main/java/com/bisson2000/everdrill/Everdrill.java
+++ src/main/java/com/bisson2000/everdrill/Everdrill.java
@@ -6,6 +6,10 @@ import com.bisson2000.everdrill.config.EverdrillConfig;
 import com.bisson2000.everdrill.entities.ModEntities;
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper;
+import com.simibubi.create.foundation.item.TooltipModifier;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -26,9 +30,18 @@ public class Everdrill
 
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "everdrill";
-    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Everdrill.MOD_ID);
+
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
+
+    // Registrate
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Everdrill.MOD_ID);
+    static {
+        REGISTRATE.setTooltipModifierFactory(item -> {
+            return new ItemDescription.Modifier(item, TooltipHelper.Palette.STANDARD_CREATE)
+                    .andThen(TooltipModifier.mapNull(KineticStats.create(item)));
+        });
+    }
 
     public Everdrill() {
         this(FMLJavaModLoadingContext.get());
